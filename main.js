f5e69f558bccb7cee655e09a3e9ba43337b4dc55
Pascal Klein <PascalKlein@gmx.net>
2021-04-04 10:53:35 +0200

    set up basic test formats

--- main.js
+++ main.js
@@ -1,25 +1,75 @@
 var game;
 
-
-function newGame()
-{
-    return {
-        screen: "education"
-    }
+const screens = {
+    EDUCATION: {
+        ACTIONS: "actions",
+        UPGRADES: "upgrades"
+    },
+    ADVENTURE: "adventure"
 }
 
 
+/**
+ * Function that creates a new game object with an empty save file
+ * @returns New blank game object
+ */
+function newGame()
+{  
+    var education_actions = [{id: 0, text: "action1", unlocked: true}, {id: 1, text: "action2", unlocked: false}]; //TODO: make screens, subscreens, and education_actions into enums
+    return {
+        topscreen: screens.EDUCATION,
+        subscreen: screens.EDUCATION.ACTIONS,
+        education_actions
+    }
+}
+
+/**
+ * Saves a game onto a save file
+ */
 function save_game()
 {
     localStorage.setItem("plethora_careers", btoa(JSON.stringify(game)));
 }
 
+/**
+ * Loads a saved game from a given save and return the loaded game object
+ * @returns Loaded game object
+ */
+function loadSaveGame(save)
+{
+    game = newGame(); //TODO: actually load a game here from the save
+    return game; 
+}
 
+/**
+ * First called function on html body's onload() that creates a game context either from a savefile or from a fresh save
+ */
 function load_game()
 {
     let save = localStorage.getItem("plethora_careers");
     if(save == null || save == undefined){
         game = newGame();
+    }else{
+        game = loadSaveGame(save); 
     }
-    init_vue(); 
+    init_vue();  //with the game object created through newGame(), a Vue environment can be created that uses the game object as its data
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- vue_init.js
+++ vue_init.js
@@ -1,9 +1,45 @@
-function init_vue()
+
+
+function define_education_components()
 {
-    var app = new Vue({
-        el:"#app",
-        data:{
-            game
+    Vue.component('test_item', {
+        props: ['actions'],
+        template: '<td v-if="actions.unlocked">{{ actions.text }}</td>'
+    })
+}
+
+
+function define_adventure_components()
+{
+
+}
+
+
+function define_misc_components()
+{
+
+}
+
+
+
+
+function define_Vue_components()
+{
+    define_education_components();
+    define_adventure_components();
+    //etc.
+    define_misc_components();
+}
+
+
+function init_vue()  //this function is called as part of an onload() function (load_game()) for the html body
+{
+    define_Vue_components(); //calls definitions for special Vue components to be used in the html to be made
+
+    var app = new Vue({ //create a new Vue environment
+        el:"#app",  //on this div
+        data:{  //with these variables we will use within this div as Vue variables
+            game  //base object that will hold game variables and context
         },
     })
 }
