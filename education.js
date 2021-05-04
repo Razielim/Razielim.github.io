ab403f45902119c4d8df92c60cca3189125b2fb0
Pascal Klein <PascalKlein@gmx.net>
2021-05-05 00:22:20 +0200

    populated education actions and refactored resources and action variables

--- education.js
+++ education.js
@@ -1,19 +1,44 @@
 
 //unlocked: can be seen by player, available: can be currently clicked by the player, active: has been clicked by the player and activated
-var education_actions = [{id: 0, text: "Do Homework", unlocked: true, active: false, available: true, curTime: 0, maxTime: 500, bpGain: 5, hwGain: -1, spGain: 0, ppGain: 0}, 
-                        {id: 1, text: "Go To School", unlocked: false, active: false, available: false, curTime: 0, maxTime: 5000, bpGain: 15, hwGain: 5, spGain: 0, ppGain: 3}, 
-                        {id: 2, text: "Take Test", unlocked: false, active: false, available: false, curTime: 0, maxTime: 2500, bpCost: 50, hwGain: 0, spGain: 0, ppGain: 20}, 
-                        {id: 3, text: "Pursue Hobby", unlocked: false, active: false, available: true, curTime: 0, maxTime: 1500, bpGain: 0, hwGain: 0, spGain: 10, ppGain: 5}];
+var doHomework = {id: 0, text: "Do Homework", unlocked: true, active: false, available: true, automatable: false, curTime: 0, maxTime: 500, bpGain: 5, hwGain: -1, spGain: 0, ppGain: 0, 
+                    tooltip: "Can only do homework when you have homework to do."};
+var goToSchool = {id: 1, text: "Go To School", unlocked: false, active: false, available: false, automatable: false, curTime: 0, maxTime: 5000, bpGain: 15, hwGain: 5, spGain: 0, ppGain: 3, 
+                    tooltip: "Can only go to school when you have no homework left.", count: 0};
+var takeTest = {id: 2, text: "Take Test", unlocked: false, active: false, available: false, automatable: false, curTime: 0, maxTime: 2500, bpGain: 50, hwGain: 0, spGain: 0, ppGain: 20, 
+                    tooltip: "Can only take test after going to school more.", count: 0};
+var pursueHobby = {id: 3, text: "Pursue Hobby", unlocked: false, active: false, available: true, automatable: false, curTime: 0, maxTime: 1500, bpGain: 0, hwGain: 0, spGain: 10, ppGain: 5, 
+                    tooltip: ""};
+var scienceExperiment = {id: 4, text: "Scientific Experiment", unlocked: false, active: false, available: true, automatable: false, curTime: 0, maxTime: 6000, bpGain: "5-25", hwGain: 0, spGain: 0, ppGain: 0, 
+                    tooltip: ""};
+var skipSchool = {id: 5, text: "Skip School", unlocked: false, active: false, available: true, automatable: false, curTime: 0, maxTime: 2500, bpGain: 0, hwGain: 5, spGain: 0, ppGain: 0, 
+                    tooltip: "Can not skip school with more than 25 homework left."};
+var pullAllNighter = {id: 6, text: "Pull an All-Nighter", unlocked: false, active: false, available: true, automatable: false, curTime: 0, maxTime: 1500, bpGain: 0, hwGain: "-100%", spGain: 0, ppGain: 0, 
+                    tooltip: "Can not pull an all-nighter at 100 Stress."};
+var masterCourse  = {id: 7, text: "Master a Course", unlocked: false, active: false, available: true, automatable: false, curTime: 0, maxTime: 5000, bpGain: 0, hwGain: 0, spGain: 150, ppGain: 0, 
+                    tooltip: "Can not master a course until you have taken more tests."};
+var repeatYear = {id: 8, text: "Repeat a Year", unlocked: false, active: false, available: true, automatable: false, curTime: 0, maxTime: 10000, bpGain: 200, hwGain: 0, spGain: 0, ppGain: -200, 
+                    tooltip: "Can not repeat a year when you have less than 200 progress points"};
+var studyAhead = {id: 9, text: "Study Ahead", unlocked: false, active: false, available: true, automatable: false, curTime: 0, maxTime: 3500, bpGain: 50, hwGain: 0, spGain: 0, ppGain: 0, 
+                    tooltip: "Can only study ahead when you have no homework left."};
+var education_actions = [doHomework, goToSchool, takeTest, pursueHobby, scienceExperiment, skipSchool, pullAllNighter, masterCourse, repeatYear, studyAhead];
 
-var education_resources = [{id: 0, text: "Brain Points", count: 0, total: 0}, 
-                        {id: 1, text: "Homework Left", count: 5}, 
-                        {id: 2, text: "Spark Points", count: 0}, 
-                        {id: 3, text: "Stress", count: 0, required: 100, rate: 2, clickDecrease: 1}];
+var bp = {id: 0, text: "Brain Points", count: 0, total: 0};
+var hw = {id: 1, text: "Homework Left", count: 5};
+var sp = {id: 2, text: "Spark Points", count: 0};
+var stress = {id: 3, text: "Stress", count: 0, required: 100, rate: 2, clickDecrease: 1};
+var education_resources = [bp, hw, sp, stress];
 
-var education_upgrades = [{id: 0, text: "Ask A Friend", unlocked: false, active: false, available: false, bpCost: 50},
-                        {id: 1, text: "Squeaky Teddy", unlocked: false, active: false, available: false, bpCost: 100}];
+var doubletime = {id: 0, text: "Work doubletime", unlocked: false, active: false, available: false, bpCost: 50, tooltip: "Finish 2 homework pieces at once for every Do Homework task."};
+var squeakyTeddy = {id: 1, text: "Squeaky Teddy", unlocked: false, active: false, available: false, bpCost: 100, tooltip: "Clicking the Stress Teddy reduces Stress by 1 additional point."};
+var askAFriend = {id: 2, text: "Ask a friend", unlocked: false, active: false, available: false, bpCost: 200, tooltip: "Unlocks the ability to Automate the Do Homework task."};
+var dailyRoutine = {id: 3, text: "Develop a daily routine", unlocked: false, active: false, available: false, bpCost: 250, tooltip: "Unlocks the ability to Automate the Go To School task."};
+var drinkBetterSoda = {id: 4, text: "Drink better soda", unlocked: false, active: false, available: false, bpCost: 350, tooltip: "Halves the rate at which Stress accumulates."};
+var getAHobby = {id: 5, text: "Get A Hobby", unlocked: false, active: false, available: false, bpCost: 500, tooltip: "Unlocks the Hobby task, which you have to give a name to first when you get this upgrade."};
+var workSmarter = {id: 6, text: "Work smarter", unlocked: false, active: false, available: false, bpCost: 600, tooltip: "Gain the Brain Points of the Do Homework task per homework completed when completing homework through other tasks."};
+var education_upgrades = [doubletime, squeakyTeddy, askAFriend, dailyRoutine, drinkBetterSoda, getAHobby, workSmarter];
 
-var education_spark_upgrades = [{id: 0, text: "NYI", unlocked: false, active: false}];
+var NYI = {id: 0, text: "NYI", unlocked: false, active: false};
+var education_spark_upgrades = [NYI];
 
 
 function swap_education_subscreen()
@@ -38,20 +63,22 @@ function load_education_screen()
 
 function update_education()
 {
+    tickMass = (1/tickrate) * 1000 * (1 - (0.66 * (stress.count/100))); //tick speed is 1000 per second, slowed down to 333 with 100 Stress; tickMass is one frame worth of tick speed
+    tickMass *= 50; //for debugging and testing
     for(action of education_actions)
     {
         if(action.active)
         {
-            action.curTime += (1/tickrate) * 1000 * (1 - (0.66 * (education_resources[3].count/100)));
+            action.curTime += tickMass;
             if(action.curTime >= action.maxTime)
             {
                 handleActionDone(action);
             }
         }
     }
-    if(education_resources[3].count < education_resources[3].required)
+    if(stress.count < stress.required)
     {
-        education_resources[3].count += education_resources[3].rate/tickrate;
+        stress.count += stress.rate/tickrate;
     }
 
     checkAvailabilitiesAndUnlocks();
@@ -60,34 +87,60 @@ function update_education()
 
 function checkAvailabilitiesAndUnlocks()
 {
-    if(!education_actions[0].available && education_resources[1].count > 0){
-        education_actions[0].available = true;
+    doHomework.available = hw.count > 0 ? true : false;
+
+    if(!goToSchool.unlocked && hw.count <= 2){ //school action unlocked when hw <= 2
+        goToSchool.unlocked = true;
     }
-    if(education_actions[0].available && education_resources[1].count <= 0){
-        education_actions[0].available = false;
+    goToSchool.available = hw.count <= 0 ? true : false;
+
+    if(!takeTest.unlocked && bp.total >= 150){ //take test action unlocked at 50+ total bp earned
+        takeTest.unlocked = true;
+    }
+    takeTest.available = goToSchool.count >= 5 ? true : false;
+
+    if(!scienceExperiment.unlocked && general_resources[0].count >= 50){
+        scienceExperiment.unlocked = true;
     }
 
-    if(!education_actions[1].unlocked && education_resources[1].count <= 2){ //school action unlocked when hw <= 2
-        education_actions[1].unlocked = true;
+    if(!skipSchool.unlocked && bp.total >= 200){
+        skipSchool.unlocked = true;
     }
-    if(!education_actions[1].available && education_resources[1].count <= 0){ //school action possible when hw <= 0
-        education_actions[1].available = true;
+    skipSchool.available = hw.count <= 25 ? true : false;
+
+    if(!pullAllNighter.unlocked && bp.total >= 300){
+        pullAllNighter.unlocked = true;
     }
+    pullAllNighter.available = stress.count < 100 ? true : false;
+
+    if(!masterCourse.unlocked && bp.total >= 500){
+        masterCourse.unlocked = true;
+    }
+    masterCourse.available = takeTest.count >= 5 ? true : false;
+
+    if(!repeatYear.unlocked && bp.total >= 800){
+        repeatYear.unlocked = true;
+    }
+    repeatYear.available = general_resources[0].count >= 200 ? true : false;
+
+    if(!studyAhead.unlocked && bp.total >= 1250){
+        studyAhead.unlocked = true;
+    }
+    studyAhead.available = hw.count <= 0 ? true : false;
 
     for(upgrade of education_upgrades)
     {
-        if(!upgrade.unlocked && education_resources[0].total >= upgrade.bpCost/2){
+        if(!upgrade.unlocked && bp.total >= upgrade.bpCost/2){
             upgrade.unlocked = true;
         }
-        if(!upgrade.available && upgrade.bpCost <= education_resources[0].count){
+        if(!upgrade.active && !upgrade.available && upgrade.bpCost <= bp.count){
             upgrade.available = true;
         }
-        if(upgrade.available && upgrade.bpCost > education_resources[0].count){
+        if(upgrade.available && upgrade.bpCost > bp.count){
             upgrade.available = false;
         }
     }
 }
-
 
 
 function handleActionDone(action)
@@ -105,9 +158,35 @@ function handleActionDone(action)
             break;
         }
         case 2:{
+            handleTestDone(action);
             break;
         }
         case 3:{
+            handleHobbyDone(action);
+            break;
+        }
+        case 4:{
+            handleExperimentDone(action);
+            break;
+        }
+        case 5:{
+            handleSkipDone(action);
+            break;
+        }
+        case 6:{
+            handleAllNighterDone(action);
+            break;
+        }
+        case 7:{
+            handleMasterCourseDone(action);
+            break;
+        }
+        case 8:{
+            handleRepeatDone(action);
+            break;
+        }
+        case 9:{
+            handleStudyDone(action);
             break;
         }
         default:{
@@ -117,75 +196,103 @@ function handleActionDone(action)
 }
 
 
-function handleHomeworkDone(action) //here, action is the homework action
+function handleHomeworkDone(action)
 {
-    //space here if special upgrades need to change behaviour
-    education_resources[1].count += action.hwGain;
-    education_resources[0].count += action.bpGain;
-    education_resources[0].total += action.bpGain;
+    hw.count += action.hwGain;
+    bp.count += action.bpGain;
+    bp.total += action.bpGain;
 
-    if(education_resources[1].count < 0){
-        education_resources[1].count = 0;
+    if(hw.count < 0){
+        hw.count = 0;
     }
 }
 
 
 function handleSchoolDone(action)
 {
-    education_resources[0].count += action.bpGain;
-    education_resources[0].total += action.bpGain;
+    bp.count += action.bpGain;
+    bp.total += action.bpGain;
     general_resources[0].count += action.ppGain;
-    education_resources[1].count += action.hwGain;
-    education_actions[1].available = false;
+    hw.count += action.hwGain;
+    action.count++;
+    goToSchool.available = false;
 }
 
+                   
+function handleTestDone(action)
+{
+    bp.count += action.bpGain;
+    bp.total += action.bpGain;
+    general_resources[0].count += action.ppGain;
+    takeTest.available = false;
+    goToSchool.count = 0;
+    action.count++;
+}
+
+function handleHobbyDone(action)
+{
+    hw.count += action.hwGain;
+    general_resources[0].count += action.ppGain;
+}
+
+function handleExperimentDone(action)
+{
+    gain = Math.floor(Math.random() * 21) + 5;
+    bp.count += gain;
+    bp.total += gain;
+}
+
+function handleSkipDone(action)
+{
+    hw.count += action.hwGain;
+    goToSchool.available = false;
+    stress.count *= 0.5;
+}
+
+function handleAllNighterDone(action)
+{
+    if(workSmarter.active){
+        bp.count += hw.count * doHomework.bpGain;
+    }
+    hw.count = 0;
+    stress.count = 100;
+}
+
+function handleMasterCourseDone(action)
+{
+    sp.count += action.spGain;
+    takeTest.count = 0;
+}
+
+function handleRepeatDone(action)
+{
+    bp.count += action.bpGain;
+    bp.total += action.bpGain;
+    general_resources[0].count += action.ppGain;
+}
+
+function handleStudyDone(action)
+{
+    bp.count += action.bpGain;
+    bp.total += action.bpGain;
+    stress.count += 10;
+}
+
+
+
+
+function clickedStressTeddy()
+{
+    if(stress.count >= 1){
+        stress.count -= stress.clickDecrease;
+    }
+}
 
 function clickedAction(action)
 {
     if(!action.available || action.active || !action.unlocked){
         return;
-    }
-    switch(action.id)
-    {
-        case 0:{
-            activateHomeworkAction(action);
-            break;
-        }
-        case 1:{
-            activateSchoolAction(action);
-            break;
-        }
-        case 2:{
-            break;
-        }
-        case 3:{
-            break;
-        }
-        default:{
-            break;
-        }
-    }
-}
-
-
-function clickedStressTeddy()
-{
-    if(education_resources[3].count >= 1){
-        education_resources[3].count -= education_resources[3].clickDecrease;
-    }
-}
-
-
-function activateHomeworkAction(action)
-{
-    if(education_resources[1].count > 0 && !education_actions[1].active){
-        action.active = true;
-    }
-}
-
-function activateSchoolAction(action)
-{
-    if(education_resources[1].count == 0){
+    }else{
         action.active = true;
     }
 }
@@ -193,50 +300,103 @@ function activateSchoolAction(action)
 
 function clickedUpgrade(upgrade)
 {
-    if(upgrade.active || !upgrade.unlocked){
+    if(upgrade.active || !upgrade.unlocked || !(upgrade.bpCost <= bp.count)){
         return;
-    }
-    switch(upgrade.id)
-    {
-        case 0:{
-            activateAskAFriendUpgrade(upgrade);
-            break;
-        }
-        case 1:{
-            activateSqueakyTeddyUpgrade(upgrade);
-            break;
-        }
-        case 2:{
-            break;
-        }
-        case 3:{
-            break;
-        }
-        default:{
-            break;
+    }else{
+
+        bp.count -= upgrade.bpCost;
+        upgrade.active = true;
+        upgrade.available = false;
+
+        switch(upgrade.id)
+        {
+            case 0:{
+                activateDoubleTimeUpgrade(upgrade);
+                break;
+            }
+            case 1:{
+                activateSqueakyTeddyUpgrade(upgrade);
+                break;
+            }
+            case 2:{
+                activateAskAFriendUpgrade(upgrade);
+                break;
+            }
+            case 3:{
+                activateDailyRoutineUpgrade(upgrade);
+                break;
+            }
+            case 4:{
+                activateDrinkBetterSodaUpgrade(upgrade);
+                break;
+            }
+            case 5:{
+                activateGetAHobbyUpgrade(upgrade);
+                break;
+            }
+            case 6:{
+                activateWorkSmarterUpgrade(upgrade);
+                break;
+            }
+            default:{
+                break;
+            }
         }
     }
 }
 
 
-function activateAskAFriendUpgrade(upgrade)
+function activateDoubleTimeUpgrade(upgrade)
 {
-    if(upgrade.bpCost <= education_resources[0].count)
-    {
-        education_resources[0].count -= upgrade.bpCost;
-        upgrade.active = true;
-
-        education_actions[0].hwGain *= 2;
-    }
+    doHomework.hwGain *= 2;
 }
 
 function activateSqueakyTeddyUpgrade(upgrade)
 {
-    if(upgrade.bpCost <= education_resources[0].count)
-    {
-        education_resources[0].count -= upgrade.bpCost;
-        upgrade.active = true;
-
-        education_resources[3].clickDecrease += 1;
-    }
+    stress.clickDecrease += 1;
 }
+
+function activateAskAFriendUpgrade(upgrade)
+{
+    doHomework.automatable = true;
+}
+
+function activateDailyRoutineUpgrade(upgrade)
+{
+    goToSchool.automatable = true;
+}
+
+function activateDrinkBetterSodaUpgrade(upgrade)
+{
+    stress.rate *= 0.5;
+}
+
+function activateGetAHobbyUpgrade(upgrade)
+{
+    bp.count += upgrade.bpCost;
+    //TODO: show hobby naming dialogue; if the user fails to complete this, return so that the bpCost doesnt get deducted
+    //pursueHobby.text = inputText;
+    bp.count -= upgrade.bpCost;
+    pursueHobby.unlocked = true;
+    pursueHobby.available = true;
+}
+
+function activateWorkSmarterUpgrade(upgrade)
+{
+    //empty, this gets checked during the completion of other actions
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- vue_init.js
+++ vue_init.js
@@ -11,7 +11,9 @@ function define_education_components()
     });
     Vue.component('stressdisplay', {
         props: ['stress'],
-        template: `<div id="stressdisplay"><div style="grid-column: 1;grid-row: 1;margin-top: 2px;">
+        template: `<div id="stressdisplay" class="tooltippable"> 
+                        <span class="tooltiptext">The higher your Stress, the slower your actions finish! Click the Teddy to reduce Stress!</span>
+                    <div style="grid-column: 1;grid-row: 1;margin-top: 2px;">
                         <img src="assets/Education/stressFill.png" v-bind:style="{width: (stress.count/stress.required)*164 + 'px', height: 23 + 'px'}"></div>
                     <point_counter_requirement style="grid-column: 1;grid-row: 1;" v-bind:item = "stress" class="centered_vertically"></point_counter_requirement></div>`
     });
@@ -19,9 +21,15 @@ function define_education_components()
         props: ['resources'],
         template: `<div id="education_sidebar" class="right_entries">
             <div id="education_sidebar_pointdisplay">
-            <div class="centered_vertically"><img src="assets/iconpack/icon_bp.png" width="30" height="30"><point_counter_standard v-bind:item = "resources[0]" style="padding-left: 10px"></div>
-            <div class="centered_vertically"><img src="assets/iconpack/icon_hw.png" width="30" height="30"><point_counter_standard v-bind:item = "resources[1]" style="padding-left: 10px"></div>
-            <div class="centered_vertically"><img src="assets/iconpack/icon_sp.png" width="30" height="30"><point_counter_standard v-bind:item = "resources[2]" style="padding-left: 10px"></div></div>
+            <div class="centered_vertically tooltippable">
+                <span class="tooltiptext" style="width: 120px;">Brain Points</span>    
+                <img src="assets/iconpack/icon_bp.png" width="30" height="30"><point_counter_standard v-bind:item = "resources[0]" style="padding-left: 10px"></div>
+            <div class="centered_vertically tooltippable">
+                <span class="tooltiptext" style="width: 150px;">Homework Left</span>
+                <img src="assets/iconpack/icon_hw.png" width="30" height="30"><point_counter_standard v-bind:item = "resources[1]" style="padding-left: 10px"></div>
+            <div class="centered_vertically tooltippable">
+                <span class="tooltiptext" style="width: 120px;">Spark Points</span>
+                <img src="assets/iconpack/icon_sp.png" width="30" height="30"><point_counter_standard v-bind:item = "resources[2]" style="padding-left: 10px"></div></div>
             <div id="education_sidebar_stressdisplay">Stress
             <stressdisplay v-bind:stress = "resources[3]"></stressdisplay></div>
             <div id="stressteddy" v-on:click="clickedStressTeddy()"></div>
@@ -36,8 +44,9 @@ function define_education_components()
             </div>`
     });
     Vue.component('education_action', {
-        props: ['action', 'resources'],     //if not action.available, gray out the button
-        template: `<div v-on:click="clickedAction(action)" v-bind:class="{education_action: true, unavailable:action.available?false:true}"> 
+        props: ['action', 'resources'],
+        template: `<div v-on:click="clickedAction(action)" v-bind:class="{education_action: true, unavailable:action.available?false:true, tooltippable: !action.available?true:false}">
+                <span v-if="!action.available" class="tooltiptext">{{action.tooltip}}</span> 
             <div class="education_action_name">{{ action.text }}</div>
             <div class="education_action_timeleft_bar"><div style="grid-column: 1;grid-row: 1;"><img src="assets/Education/stressFill.png" v-bind:style="{width: (action.curTime/action.maxTime)*225 + 'px', height: 24 + 'px'}"></div>
             <div style="grid-column: 1;grid-row: 1;">{{ Math.round(action.curTime) }}/{{ action.maxTime }}</div></div>
@@ -46,7 +55,7 @@ function define_education_components()
         </div>`
     });
     Vue.component('education_upgrade', {
-        props: ['upgrade', 'resources'],     //if upgrade not affordable, gray out the button
+        props: ['upgrade', 'resources'],
         template: `<div v-on:click="clickedUpgrade(upgrade)" v-bind:class="{education_upgrade: true, unavailable:upgrade.available?false:true}"> 
             <div class="education_upgrade_name">{{ upgrade.text }}</div>
             <div class="centered_vertically" style="margin-right: 5px;"><img src="assets/iconpack/icon_bp.png" width="28" height="28">  {{upgrade.bpCost}} </div>
@@ -73,8 +82,10 @@ function define_misc_components()
     });
     Vue.component('top_pane_holder', {
         template: `<div><div class="left_entries"><div>Stage 1: Educate yourself!</div> <div>Clear Reward: First Career Choice</div>
-            </div><div class="right_entries"><div class="centered_vertically"><img src="assets/iconpack/icon_pp.png" width="30" height="30" class="left_entries">
-            <point_counter_requirement v-bind:item = "game.resources[0][0]"></point_counter_requirement></div></div>`
+            </div><div class="right_entries centered_vertically tooltippable">
+                <span class="tooltiptext" style="width: 120px;">Progress Points</span>
+            <img src="assets/iconpack/icon_pp.png" width="30" height="30" class="left_entries">
+            <point_counter_requirement v-bind:item = "game.resources[0][0]"></point_counter_requirement></div>`
     });
 }
 
